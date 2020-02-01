3ae2484dd1e58ca8a9a12def2b83b09807748cfb
DESKTOP-CM4DN2T\joncatlin <jonc@destini.com>
2020-02-01 14:11:08 -0700

    Added resource constraints to the compose file. Removed crud from the startup.sh file.

--- crushftp/startup.sh
+++ crushftp/startup.sh
@@ -1,15 +1,9 @@
-#if [[ -f /tmp/CrushFTP9_PC.zip ]] ; then
-#    echo "Unzipping CrushFTP..."
-#    unzip -o -q /tmp/CrushFTP9_PC.zip -d /var/opt/
-#    rm -f /tmp/CrushFTP9_PC.zip
-#fi
-
+# If there is no crushadmin user then create one with a default password
 if [[ ! -d /var/opt/CrushFTP9/users/MainUsers/crushadmin ]] ; then
     echo "Creating default admin..."
     cd /var/opt/CrushFTP9 && java -jar /var/opt/CrushFTP9/CrushFTP.jar -a "crushadmin" "crushadmin"
 fi
 
+# Start the crush service
 echo "Starting crush..."
 java -Xmx1024m -jar CrushFTP.jar -d
-# /var/opt/run-crushftp.sh start
-#while true; do sleep 86400; done
